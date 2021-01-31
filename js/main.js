8c6a68d2728cf496c6c1874180d50f0bef5a1d25
EvgenKalinin <edvard2k5@mail.ru>
2021-01-31 18:33:44 +0300

    Добавляет функции для получения рандомного числа из диапазона

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// Вернет целое число из диапазона включительно.
+
+const getRandom = (min, max) => {
+  if (min >= max || min < 0 || max < 0) {
+    return alert('Введите корректные значения. Минимально значение не может быть больше максимального. Значения должны быть положительными.');
+  }
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+console.log('Твое целое число: ' + getRandom(4, 5));
+
+// Вернет дробное число из диапазона включительно.
+
+const getRandomCoordinates = (min, max, digits = 1) => {
+  if (min >= max || min < 0 || max < 0) {
+    return alert('Введите корректные значения. Минимально значение не может быть больше максимального. Значения должны быть положительными.');
+  }
+  return (Math.random() * (max - min) + min).toFixed(digits);
+}
+
+console.log('Твое дробное число: ' + getRandomCoordinates(4, 5));
